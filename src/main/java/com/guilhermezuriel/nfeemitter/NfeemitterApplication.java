26efd8035c3fd4d50abb26b270caa97278983eb6
Guilherme Zuriel <guizuriel.dev@gmail.com>
2026-03-18 11:01:19 -0300

    first commit

---
+++ src/main/java/com/guilhermezuriel/nfeemitter/NfeemitterApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermezuriel.nfeemitter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfeemitterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfeemitterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guilhermezuriel/nfeemitter/NfeemitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guilhermezuriel.nfeemitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfeemitterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
